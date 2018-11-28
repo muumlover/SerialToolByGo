20e328217f721784c52c9a19cfb98ce2a748843a
muumlover <muumlover@live.com>
2018-11-28 23:20:38 +0800

    async

--- serial_goroutine.go
+++ serial_goroutine.go
@@ -3,10 +3,13 @@ package main
 import (
 	"github.com/tarm/serial"
 	"log"
+	"time"
 )
 
 func main() {
-	c := &serial.Config{Name: "COM1", Baud: 115200}
+	loop := true
+
+	c := &serial.Config{Name: "COM6", Baud: 115200}
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		log.Fatal(err)
@@ -17,13 +20,30 @@ func main() {
 		log.Fatal(err)
 	}
 
+	var recv []byte
 	go func(s *serial.Port) {
-		buf := make([]byte, 128)
-		n, err = s.Read(buf)
-		if err != nil {
-			log.Fatal(err)
+		for loop {
+			buf := make([]byte, 1024)
+			n, err = s.Read(buf)
+			if err != nil {
+				log.Fatal(err)
+			}
+			recv = append(recv, buf[:n]...)
 		}
-		log.Print("%q", buf[:n])
 	}(s)
+	go func() {
+		for loop {
+			if len(recv) > 0 {
+				time.Sleep(10)
+				print(string(recv))
+				recv = *new([]byte)
+			}
+		}
+	}()
+
+	for loop {
+		a := 1
+		a++
+	}
 	print("exit")
 }
