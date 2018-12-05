ff8b68c4a5ab39cd2169d4b5d5b6e526028403aa
muumlover <muumlover@live.com>
2018-12-05 18:22:29 +0800

    init

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/StackExchange/wmi"
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
+	"github.com/lxn/win"
 	"github.com/tarm/serial"
 	"log"
 	"strconv"
@@ -24,7 +25,7 @@ func getPortNameList() []PortNameItem {
 	q := wmi.CreateQuery(&dst, "WHERE Name LIKE '%(COM%)'")
 	err := wmi.Query(q, &dst)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 	portList := make([]PortNameItem, len(dst))
 	for i, v := range dst {
@@ -116,37 +117,60 @@ type mwMainWindow struct {
 	cbStopBits     *walk.ComboBox
 	btnSerialOpen  *walk.PushButton
 	txtSerialState *walk.Label
+	txtSerialRecv  *walk.TextEdit
+	txtSerialSend  *walk.TextEdit
 
 	scItems SerialConfigItems
 	sc      *serial.Config
+	sp      *serial.Port
 }
 
-func (mw *mwMainWindow) openSerial() {
-	println(mw.sc.Name)
-	println(mw.sc.Baud)
-	println(mw.sc.Size)
-	println(mw.sc.Parity)
-	println(mw.sc.StopBits)
-	print(mw.cbSerialPort.BindingMember())
-	s, err := serial.OpenPort(mw.sc)
+func (mw *mwMainWindow) openSerial() error {
+	sc := serial.Config{}
+	sc.Name = mw.cbSerialPort.Value().(string)
+	sc.Baud = mw.cbBaudRate.Value().(int)
+	sc.Size = mw.cbDataBits.Value().(byte)
+	sc.Parity = mw.cbParity.Value().(serial.Parity)
+	sc.StopBits = mw.cbStopBits.Value().(serial.StopBits)
+	s, err := serial.OpenPort(&sc)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-
+	mw.sp = s
 	n, err := s.Write([]byte("test"))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	go func(s *serial.Port) {
-		buf := make([]byte, 128)
-		n, err = s.Read(buf)
-		if err != nil {
-			log.Fatal(err)
+		for {
+			buf := make([]byte, 10240)
+			n, err = s.Read(buf)
+			if err != nil {
+				log.Print(err)
+				break
+			}
+			// noinspection ALL
+			log.Print(buf[:n])
+			str := string(buf[:n])
+			log.Print(str)
+			str = strings.Replace(str, "\x00", "", -1)
+			mw.txtSerialRecv.AppendText(str)
+			if err != nil {
+				log.Print(err)
+				break
+			}
 		}
-		// noinspection ALL
-		log.Print("%q", buf[:n])
 	}(s)
+
+	return nil
+}
+
+func (mw *mwMainWindow) closeSerial() error {
+	if err := mw.sp.Close(); err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
@@ -158,6 +182,7 @@ func main() {
 		AssignTo: &mw.MainWindow,
 		Title:    Bind("'Animal Details' + (sc.Name == '' ? '' : ' - ' + sc.Name)"),
 		MinSize:  Size{Width: 600, Height: 400},
+		Visible:  false,
 		Layout:   HBox{},
 		Children: []Widget{
 			Composite{
@@ -270,7 +295,11 @@ func main() {
 												Name:     "SerialOpen",
 												Text:     Bind("SerialState.Text=='OFF'?'打开串口':'关闭串口'"),
 												OnClicked: func() {
-													if mw.btnSerialOpen.Text() == "关闭串口" {
+													if mw.txtSerialState.Text() == "ON" {
+														if err := mw.closeSerial(); err != nil {
+															log.Print(err)
+															return
+														}
 														bg, err := walk.NewSolidColorBrush(walk.RGB(255, 0, 0))
 														if err != nil {
 															log.Print(err)
@@ -281,6 +310,10 @@ func main() {
 															return
 														}
 													} else {
+														if err := mw.openSerial(); err != nil {
+															log.Print(err)
+															return
+														}
 														bg, err := walk.NewSolidColorBrush(walk.RGB(0, 255, 0))
 														if err != nil {
 															log.Print(err)
@@ -320,13 +353,15 @@ func main() {
 				Layout: VBox{MarginsZero: true},
 				Children: []Widget{
 					TextEdit{
-						Name: "textRecv",
-						Text: Bind("textSend.Text"),
+						Name:     "txtSerialRecv",
+						AssignTo: &mw.txtSerialRecv,
+						ReadOnly: true,
+						VScroll:  true,
 					},
 					VSpacer{},
 					TextEdit{
-						Name: "textSend",
-						Text: "",
+						Name:     "txtSerialSend",
+						AssignTo: &mw.txtSerialSend,
 					},
 				},
 			},
@@ -337,7 +372,18 @@ func main() {
 			},
 		},
 	}).Create(); err != nil {
-		log.Fatal(err)
+		log.Print(err)
+	}
+	screenX := int(win.GetSystemMetrics(win.SM_CXSCREEN))
+	screenY := int(win.GetSystemMetrics(win.SM_CYSCREEN))
+	if err := mw.MainWindow.SetBounds(walk.Rectangle{
+		X:      (screenX - mw.Width()) / 2,
+		Y:      (screenY - mw.Height()) / 2,
+		Width:  mw.Width(),
+		Height: mw.Height(),
+	}); err != nil {
+		log.Print(err)
+		return
 	}
 	if err := mw.cbSerialPort.SetCurrentIndex(0); err != nil {
 		log.Print(err)
@@ -359,5 +405,6 @@ func main() {
 		log.Print(err)
 		return
 	}
+	mw.MainWindow.SetVisible(true)
 	mw.MainWindow.Run()
 }
